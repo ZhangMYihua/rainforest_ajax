ee396b8f3c379d9b0af844a57d7bd19367703eb2
zhangmyihua <michaelzhang2@msn.com>
2015-02-26 17:51:41 -0500

    cleaned up the code for infinite scroll and pagination. Located the double request in applications js and moved it to products js.

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,8 +1,12 @@
+
 $(document).on('ready page:load', function() {
-	$(window).scroll(function(){
-		if($(window).scrollTop() > $(document).height() - $(window).height() - 50){
-			console.log($('.pagination span.next').children().attr('href'));
-			$.getScript($('.pagination span.next').children().attr('href'));
-		};
-	});
+  if ($('.pagination').length) {
+    $(window).scroll(function() {
+      var url = $('.pagination span.next').children().attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+        $('.pagination').text("Fetching more products...");
+        return $.getScript(url);
+      }
+    });
+  }
 });
